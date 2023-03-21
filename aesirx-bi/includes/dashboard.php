8e1965f849d7cba22160f433790777c6b52f8e3a
bao <nguyenbao10ht@gmail.com>
2023-03-21 15:23:17 +0700

    add plugin

--- aesirx-bi/includes/dashboard.php
+++ aesirx-bi/includes/dashboard.php
@@ -6,11 +6,13 @@ echo "<h1>AesirX Dasboard</h1>";
 
 ?>
 
-<script>
-    window.env = [];
-    window.env.REACT_APP_CLIENT_ID = "<?php echo $options['aesirx_bi_domain_react_app_client_id'] ?>";
-    window.env.REACT_APP_CLIENT_SECRET = "<?php echo $options['aesirx_bi_domain_react_app_client_secret'] ?>";
-    window.env.REACT_APP_ENDPOINT_URL = "<?php echo $options['aesirx_bi_domain_react_app_endpoint_url'] ?>";
-    window.env.REACT_APP_LICENSE = "<?php echo $options['aesirx_bi_domain_react_app_license'] ?>";
-    window.env.REACT_APP_DATA_STREAM = "<?php echo $options['aesirx_bi_domain_react_app_data_stream'] ?>";
-</script>
+<head>
+    <script>
+        window.env = {};
+        window.env.REACT_APP_CLIENT_ID = "<?php echo $options['aesirx_bi_domain_react_app_client_id'] ?>";
+        window.env.REACT_APP_CLIENT_SECRET = "<?php echo $options['aesirx_bi_domain_react_app_client_secret'] ?>";
+        window.env.REACT_APP_ENDPOINT_URL = "<?php echo $options['aesirx_bi_domain_react_app_endpoint_url'] ?>";
+        window.env.REACT_APP_LICENSE = "<?php echo $options['aesirx_bi_domain_react_app_license'] ?>";
+        window.env.REACT_APP_DATA_STREAM = "<?php echo $options['aesirx_bi_domain_react_app_data_stream'] ?>";
+    </script>
+</head>
